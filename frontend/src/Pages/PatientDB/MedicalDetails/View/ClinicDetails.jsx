477e59b1600a4bb71383844c3a1978766534ecef
SamzGTR2000 <130070758+SamzGTR2000@users.noreply.github.com>
2024-03-23 19:23:27 +0530

    commit four

--- frontend/src/Pages/PatientDB/MedicalDetails/View/ClinicDetails.jsx
+++ frontend/src/Pages/PatientDB/MedicalDetails/View/ClinicDetails.jsx
@@ -6,8 +6,6 @@ import { faCalendarAlt } from "@fortawesome/free-solid-svg-icons";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 import axios from "axios";
-import { Navigate, useNavigate } from "react-router-dom";
-
 
 const ClinicDetailsView = ({patientId}) => {
 
@@ -24,20 +22,11 @@ const ClinicDetailsView = ({patientId}) => {
                 console.error(`${error.message}`)
             }
         }
-        fetchClinicData(patientId)
+        if (patientId) {
+            fetchClinicData(patientId);
+        }
     }, [patientId])
 
-    const handleSearchClinicDetails = async (value) => {
-        
-        try {
-            const response = await axios.get(`http://localhost:8080/searchclinicdetails/${value}`)
-
-            setSearchClinicDetails([response.data])
-            console.log(searchClinicDetails)
-        } catch (error) {
-            console.error(`${error.message}`)
-        }
-    }
 
     return (
         <div className="exam-details-section">
@@ -48,16 +37,16 @@ const ClinicDetailsView = ({patientId}) => {
             </div>
             <div className="label-value-pair">
                 <span className="label">Consultant ID:</span>
-                <span className="value">{searchClinicDetails[0][0]?.consultantId}</span>
+                <span className="value">{searchClinicDetails[0]?.[0]?.consultantId}</span>
             </div>
             <div className="label-value-pair">
                 <span className="label">Date:</span>
-                <span className="value">{searchClinicDetails[0][0]?.clinicDate}
-                </span>
+                <span className="value">{searchClinicDetails[0]?.[0]?.clinicDate}</span>
+                
             </div>
             <div className="label-value-pair">
                 <span className="label">Time:</span>
-                <span className="value">{searchClinicDetails[0][0]?.clinicTime}</span>
+                <span className="value">{searchClinicDetails[0]?.[0]?.clinicTime}</span>
             </div>
         </div>
     )
