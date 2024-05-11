d65838d323e76b7f933b75adfb01e6d4c426880d
Pabasara Samarakoon <pabasarasamarakoon2000@gmail.com>
2024-05-11 17:40:25 +0530

    Update app.js new

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ app.use(cors())
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'Dhoomiii@2000',
     database: 'eye_clinic_database'
 }).promise()
 
